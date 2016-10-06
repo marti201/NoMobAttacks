67a50bbf104f72dc9e9a28eef97f78e5d1ebc289
Marti201 <marti201@users.noreply.github.com>
2016-10-06 17:40:39 +0300

    Initial upload

---
+++ NoMobAttacks/src/me/marti201/nomobattacks/NoMobAttacks.java
@@ -0,0 +1,20 @@
+package me.marti201.nomobattacks;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoMobAttacks extends JavaPlugin implements Listener {
+
+	public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onTarget(EntityTargetLivingEntityEvent e){
+		if(e.getTarget() != null && e.getTarget().getType() == EntityType.PLAYER)
+			e.setCancelled(true);
+	}
+}
