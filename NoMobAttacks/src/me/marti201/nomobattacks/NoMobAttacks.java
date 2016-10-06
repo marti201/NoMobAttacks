86e9b9c88bdcd8aa5eddc22bc21941189ff48fd9
Marti201 <marti201@users.noreply.github.com>
2016-10-06 17:58:03 +0300

    Added permissions

--- NoMobAttacks/src/me/marti201/nomobattacks/NoMobAttacks.java
+++ NoMobAttacks/src/me/marti201/nomobattacks/NoMobAttacks.java
@@ -1,6 +1,7 @@
 package me.marti201.nomobattacks;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
@@ -14,7 +15,17 @@ public class NoMobAttacks extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onTarget(EntityTargetLivingEntityEvent e){
-		if(e.getTarget() != null && e.getTarget().getType() == EntityType.PLAYER)
-			e.setCancelled(true);
+		
+		if(e.getTarget() != null && e.getTarget().getType() == EntityType.PLAYER){
+			
+			Player player = (Player) e.getTarget();
+			
+			if(player.hasPermission("nomobattacks.noattack"))
+				e.setCancelled(true);
+			
+		}
+		
 	}
+	
+	
 }
